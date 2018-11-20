e492819c10d8d61650495b931535c94ffb745719
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-20 19:46:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-
-
-function loadIssue() {
-  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  for(var i=0;i<issues.length;i++) {
-    var result = template(issues[i]);
-    document.getElementsByTagName("main")[0].innerHTML += result;
-  }
-}
-
 function loadIssues() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
   var result = template(issues);
